7a9501426b0a977a67aa32cf84b390391dd1a791
Navneet Singh <mufc.navneet@gmail.com>
2023-08-26 00:10:30 +0530

    comments added

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const message = document.querySelector("#message");
 function numberCheckHandler() {
 
     const userDobValue = userDob.value;
-    const newUserDobValue = userDobValue.replaceAll('-', '0');
+
+    //removes special character like '/'
+    const newUserDobValue = userDobValue.replaceAll('-', '');
+
     const userNumValue = userNum.value;
 
+    //We split this numeric string into an array of individual digits.
     const digits = newUserDobValue.split("").map(Number);
 
+    console.log(digits);
 
-
-    // Calculate the sum of the digits
+    // We use the reduce function to calculate the sum of the digits, initializing the sum to 0.
     var sum = digits.reduce(function (a, b) {
         return a + b;
     }, 0);

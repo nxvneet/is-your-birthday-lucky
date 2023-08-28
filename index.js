2ade859320cc03ee4ae74b787d6d9b3c57ea100f
Navneet Singh <mufc.navneet@gmail.com>
2023-08-28 16:36:47 +0530

    validation done

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const userNum = document.querySelector("#user-num");
 const btnCheck = document.querySelector("#btn-check");
 const message = document.querySelector("#message");
 
-const userDobValue = userDob.value;
-const userNumValue = userNum.value;
 
 
 function numberCheckHandler() {
+    const userDobValue = userDob.value;
+    const userNumValue = userNum.value;
 
     //removes special character like '/'
     const newUserDobValue = userDobValue.replaceAll('-', '');
@@ -15,38 +15,31 @@ function numberCheckHandler() {
     //We split this numeric string into an array of individual digits.
     const digits = newUserDobValue.split("").map(Number);
 
-
     // We use the reduce function to calculate the sum of the digits, initializing the sum to 0.
     var sum = digits.reduce(function (a, b) {
         return a + b;
     }, 0);
 
-
-    validation();
-
-    if (sum % userNumValue == 0) {
-        setMessage(" is a lucky number!!!");
+    if (userDobValue == "" || userNumValue == "") {
+        message.innerText = "Please fill both fields";
+        return;
     }
+
     else {
-        setMessage(" is not that lucky");
+        if (sum % userNumValue == 0) {
+            message.innerText = userNumValue + " is a lucky number!!!";
+        }
+        else {
+            message.innerText = userNumValue + " is not that lucky";
+        }
     }
-
 
 }
 
-function validation() {
-
-    if (userDobValue == "" && userNumValue == "") {
-        setMessage(" Please fill both the fields");
-        return
-    }
-    console.log("click2")
-}
 
 
 function setMessage(msg) {
     message.innerText = userNumValue + msg;
-    console.log("clicked");
 }
 
 btnCheck.addEventListener("click", numberCheckHandler);
